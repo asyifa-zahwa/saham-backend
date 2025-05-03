8c1924b468440d7b3a1f829e1c53e34698f3578a
Asyifa-Zahwa <79799179+asyifa-zahwa@users.noreply.github.com>
2025-05-03 15:39:10 +0700

    first commit

---
+++ saham/src/main/java/com/almuhsin/saham/SahamApplication.java
@@ -0,0 +1,13 @@
+package com.almuhsin.saham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahamApplication.class, args);
+	}
+
+}
---
+++ saham/src/test/java/com/almuhsin/saham/SahamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almuhsin.saham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
